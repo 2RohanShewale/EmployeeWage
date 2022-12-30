1d405364fe0275d5b02bd348e45e9f3a7e152363
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 03:09:31 +0530

    UC1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Constants
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0,2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            WriteLine("Employee is present");
+        }
+        else
+        {
+            WriteLine("Empployee is absent");
+        }
+    }
+}
