f0e18db377f79234216643e43fce208d2ed3db7d
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 03:26:29 +0530

    PartTime

--- Program.cs
+++ Program.cs
@@ -4,19 +4,22 @@ class Program
     static void Main(string[] args)
     {
         //Constants
-        int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;        int EMP_RATE_PER_HOUR = 20;
 
         //variables
         Random random = new Random();
         int empHrs = 0;
-        
         int empWage = 0;
         //Computation
-        int empCheck = random.Next(0,2);
+        int empCheck = random.Next(3);
         if (empCheck == IS_FULL_TIME)
         {
             empHrs = 8;
+        }
+        else if(empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
         }
         else
         {
