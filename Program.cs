eaf69c1092fb3c05612ae44c5ab37788863a6c3f
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 03:17:44 +0530

    Employee Wage

--- Program.cs
+++ Program.cs
@@ -5,16 +5,24 @@ class Program
     {
         //Constants
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        //variables
         Random random = new Random();
+        int empHrs = 0;
+        
+        int empWage = 0;
         //Computation
         int empCheck = random.Next(0,2);
         if (empCheck == IS_FULL_TIME)
         {
-            WriteLine("Employee is present");
+            empHrs = 8;
         }
         else
         {
-            WriteLine("Empployee is absent");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        WriteLine("Emp Wage: " + empWage);
     }
 }
