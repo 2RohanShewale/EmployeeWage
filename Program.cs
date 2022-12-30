3ee90ec20eb67eaa06eb11f669df9aa480347a5c
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 03:41:16 +0530

    SwitchCase

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ class Program
     static void Main(string[] args)
     {
         //Constants
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;        int EMP_RATE_PER_HOUR = 20;
+        const int IS_FULL_TIME = 2;
+        const int IS_PART_TIME = 1;        const int EMP_RATE_PER_HOUR = 20;
 
         //variables
         Random random = new Random();
@@ -13,17 +13,11 @@ class Program
         int empWage = 0;
         //Computation
         int empCheck = random.Next(3);
-        if (empCheck == IS_FULL_TIME)
+        switch (empCheck)
         {
-            empHrs = 8;
-        }
-        else if(empCheck == IS_PART_TIME)
-        {
-            empHrs = 4;
-        }
-        else
-        {
-            empHrs = 0;
+            case IS_FULL_TIME: empHrs = 8; break;
+            case IS_PART_TIME: empHrs|= 4; break;
+            default:empHrs = 0; break;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         WriteLine("Emp Wage: " + empWage);
