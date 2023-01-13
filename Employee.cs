30382152673b346f24e11fb3c215a875b6e38177
2RohanShewale <shewalerohan50@gmail.com>
2023-01-13 20:50:04 +0530

    OOp

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Employee
+    {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+        public int empHrs = 0, totalDays = 0;
+        public  int totalEmpWage = 0, totalEmpHrs = 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,27 @@
-﻿using static System.Console;
+﻿using EmployeeWageProblem;
+using static System.Console;
 class Program
 {
-    public const int IS_FULL_TIME = 2;
-    public const int IS_PART_TIME = 1; 
-    public const int EMP_RATE_PER_HOUR = 20;
-    public const int NUM_OF_WORKING_DAYS = 20;
-    public const int MAX_HRS_IN_MONTH = 100;
+
     static void Main(string[] args)
     {
-        int empHrs = 0; int totalDays = 0;
-        int totalEmpWage = 0 , totalEmpHrs = 0;
+        Employee emp = new Employee();
         //Computation
-        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalDays <= NUM_OF_WORKING_DAYS)
+        while(emp.totalEmpHrs <= Employee.MAX_HRS_IN_MONTH || emp.totalDays <= Employee.NUM_OF_WORKING_DAYS)
         {
-            totalDays++;
+            emp.totalDays++;
             Random random = new Random();
             int empCheck = random.Next(3);
             switch (empCheck)
             {
-                case IS_FULL_TIME: empHrs = 8; break;
-                case IS_PART_TIME: empHrs = 4; break;
-                default: empHrs = 0; break;
+                case Employee.IS_FULL_TIME: emp.empHrs = 8; break;
+                case Employee.IS_PART_TIME: emp.empHrs = 4; break;
+                default: emp.empHrs = 0; break;
             }
-            totalEmpHrs += empHrs;
-            WriteLine("Day#: " + totalDays +" EMP HRS : " + empHrs);
+            emp.totalEmpHrs += emp.empHrs;
+            WriteLine("Day#: " + emp.totalDays +" EMP HRS : " + emp.empHrs);
         }
-        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        WriteLine("Total Emp Wage : " + totalEmpWage);
+        emp.totalEmpWage = emp.totalEmpHrs * Employee.EMP_RATE_PER_HOUR;
+        WriteLine("Total Emp Wage : " + emp.totalEmpWage);
     }
 }
